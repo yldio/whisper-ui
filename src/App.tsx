2ddfd7d3196008de0bf4559b677df74c4ae1b39c
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2023-05-30 22:28:47 +0100

    feat: completed UI for `v1.0`

--- src/App.tsx
+++ src/App.tsx
@@ -1,111 +1,114 @@
+import { invoke } from '@tauri-apps/api/tauri';
+import { appWindow } from '@tauri-apps/api/window';
 import {
-  Component,
-  For,
   createEffect,
   createResource,
   createSignal,
-} from "solid-js";
-import logo from "./assets/logo.svg";
-import { invoke } from "@tauri-apps/api/tauri";
-import { appWindow } from "@tauri-apps/api/window";
-import "./App.css";
-import { createFileUploader } from "@solid-primitives/upload";
-
-const SingleFileUpload: Component = () => {
-  const { files, selectFiles } = createFileUploader();
-  const { files: filesAsync, selectFiles: selectFilesAsync } =
-    createFileUploader();
-
-  return (
-    <div>
-      <div>
-        <h5>Select a single file</h5>
-        <button
-          onClick={() => {
-            selectFiles(([files]) => {
-              console.log(files);
-            });
-          }}
-        >
-          Select
-        </button>
-        <For each={files()}>{(file) => <p>{file.name}</p>}</For>
-      </div>
-
-      <div>
-        <h5>Select a single file with async callback</h5>
-        <button
-          onClick={() => {
-            selectFilesAsync(async ([{ source, name, size, file }]) => {
-              console.log({ source, name, size, file });
-            });
-          }}
-        >
-          Select
-        </button>
-        <For each={filesAsync()}>{(file) => <p>{file.name}</p>}</For>
-      </div>
-    </div>
-  );
-};
+  For,
+  onMount,
+  Show,
+} from 'solid-js';
+import Upload from './assets/upload.svg?component-solid';
+import Footer from './components/Footer';
+import Layout from './components/Layout';
+import Loading from './components/Loading';
 
 function App() {
-  const [greetMsg, setGreetMsg] = createSignal<string[]>([]);
-  const [setupRunning, setSetupRunning] = createSignal(false);
-  const [count, setCount] = createSignal(0);
+  const [messages, setMessages] = createSignal<string[]>([]);
+  const [running, setRunning] = createSignal(false);
+  const [hovering, setHovering] = createSignal(false);
+  let outputRef!: HTMLDivElement;
 
   createResource(
     async () =>
-      await appWindow.listen<{ message: string }>("setup", (ev) => {
-        setGreetMsg((prev) => [...prev, ev.payload.message]);
-      })
+      await appWindow.listen<{ message: string }>('setup', (ev) => {
+        setMessages((prev) => [...prev, ev.payload.message]);
+      }),
+  );
+  createResource(
+    async () =>
+      await appWindow.listen<{ message: string }>('run', (ev) => {
+        setMessages((prev) => [...prev, ev.payload.message]);
+      }),
   );
 
+  createResource(
+    async () =>
+      await appWindow.onFileDropEvent(async (event) => {
+        if (event.payload.type === 'hover') {
+          setHovering(true);
+        } else if (event.payload.type === 'drop') {
+          setHovering(false);
+          setMessages([]);
+          setRunning(true);
+          event.payload.paths.map(
+            async (path) => void invoke('run', { appWindow, path }),
+          );
+        } else {
+          setHovering(false);
+        }
+      }),
+  );
+
+  onMount(() => {
+    setInterval(
+      () => running() && (outputRef.scrollTop = outputRef.scrollHeight),
+      1000,
+    );
+  });
+
   const setup = async () => {
-    setSetupRunning(true);
-    const completed = await invoke("test", { appWindow });
-    setCount(count() + 1);
+    setRunning(true);
+    void invoke('setup', { appWindow, ffmpeg: true });
   };
 
   createEffect(async () => {
-    if (greetMsg().findLast((msg) => msg !== "Whisper setup complete ✅")) {
-      console.log("olé" + count());
+    if (messages().findLast((msg) => msg === 'Whisper setup complete ✅')) {
+      setRunning(false);
+    } else if (messages().findLast((msg) => msg === 'Success! 🎉')) {
+      setRunning(false);
     }
   });
 
   return (
-    <div class="container">
-      <h1>Welcome to Tauri!</h1>
+    <Layout hovering={hovering()}>
+      <Show
+        when={!running()}
+        fallback={<h1 class="pt-4 text-xl">Running...</h1>}
+      >
+        <h1 class="pt-4 text-xl">Drop a file anywhere to run</h1>
+      </Show>
 
-      <div class="row">
-        <a href="https://solidjs.com" target="_blank">
-          <img src={logo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and Solid logos to learn more.</p>
-
-      <div class="row">
-        <button type="button" onClick={() => !setupRunning() && setup()}>
+      <div class="absolute right-5 top-5">
+        <button
+          class="rounded bg-aquamarine-500 px-3 py-2 font-semibold text-blackrock-950"
+          type="button"
+          onClick={() => !running() && setup()}
+        >
           Setup
         </button>
       </div>
 
-      <p>
-        Count:
-        {count()}
-      </p>
+      <div class="mx-auto mb-1 mt-5 h-24 w-max">
+        <Show when={!running()} fallback={<Loading />}>
+          <Upload
+            fill="white"
+            class="absolute bottom-0 left-0 right-0 top-0 m-auto h-20"
+          />
+        </Show>
+      </div>
 
-      <SingleFileUpload />
+      <Show when={running()}>
+        <div
+          class="no-scrollbar mx-6 h-36 overflow-y-auto bg-dovegray font-mono"
+          ref={outputRef}
+        >
+          <For each={messages()}>{(msg) => <p>{msg}</p>}</For>
+        </div>
+      </Show>
 
-      <For each={greetMsg()}>
-        {(msg) => (
-          <div class="row">
-            <p>{msg}</p>
-          </div>
-        )}
-      </For>
-    </div>
+      <Footer />
+    </Layout>
   );
 }
 
