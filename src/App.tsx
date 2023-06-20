5e0b982fc87a9fc843f50e2cd1bbbabdee0563f8
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2023-06-20 03:42:32 +0100

    chore: fix solidjs state

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,13 @@ function App() {
   const [messages, setMessages] = createSignal<string[]>([]);
   const [running, setRunning] = createSignal(false);
   const [hovering, setHovering] = createSignal(false);
+  const [disabledSetup, setDisabledSetup] = createSignal(false);
   let outputRef!: HTMLDivElement;
+
+  const setup = async () => {
+    setRunning(true);
+    void invoke('setup', { appWindow });
+  };
 
   createResource(
     async () =>
@@ -37,7 +43,7 @@ function App() {
       await appWindow.onFileDropEvent(async (event) => {
         if (event.payload.type === 'hover') {
           setHovering(true);
-        } else if (event.payload.type === 'drop') {
+        } else if (event.payload.type === 'drop' && !running()) {
           setHovering(false);
           setMessages([]);
           setRunning(true);
@@ -57,15 +63,13 @@ function App() {
     );
   });
 
-  const setup = async () => {
-    setRunning(true);
-    void invoke('setup', { appWindow, ffmpeg: true });
-  };
-
   createEffect(async () => {
     if (messages().findLast((msg) => msg === 'Whisper setup complete ✅')) {
+      setMessages([]);
       setRunning(false);
+      setDisabledSetup(true);
     } else if (messages().findLast((msg) => msg === 'Success! 🎉')) {
+      setMessages([]);
       setRunning(false);
     }
   });
@@ -79,15 +83,19 @@ function App() {
         <h1 class="pt-4 text-xl">Drop a file anywhere to run</h1>
       </Show>
 
-      <div class="absolute right-5 top-5">
-        <button
-          class="rounded bg-aquamarine-500 px-3 py-2 font-semibold text-blackrock-950"
-          type="button"
-          onClick={() => !running() && setup()}
-        >
-          Setup
-        </button>
-      </div>
+      <Show when={!disabledSetup()}>
+        <div class="absolute right-5 top-5">
+          <button
+            class="rounded bg-aquamarine-500 px-3 py-2 font-semibold text-blackrock-950"
+            type="button"
+            onClick={() => {
+              if (!running()) setup();
+            }}
+          >
+            Setup
+          </button>
+        </div>
+      </Show>
 
       <div class="mx-auto mb-1 mt-5 h-24 w-max">
         <Show when={!running()} fallback={<Loading />}>
